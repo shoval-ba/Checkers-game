323060a4d236c67d7076264e503e0fa0574a7b27
shoval2000 <shovalshoval31@gmail.com>
2022-05-01 11:16:46 +0300

    show possible move and move the pieces

--- checkersGame.js
+++ checkersGame.js
@@ -5,10 +5,51 @@ const BLACK_PLAYER = 'black';
 let pieces = [];
 let table;
 let cell;
+let selectedCell;
+let pieceOld = null;
 
 class BoardData {
     constructor(pieces) {
         this.pieces = pieces;
+        this.turn = WHITE_PLAYER;
+    }
+
+    // Returns piece in row, col, or undefined if not exists.
+    getPiece(row, col) {
+        for (let piece of this.pieces) {
+            if (piece.row === row && piece.col === col) {
+                return piece;
+            }
+        }
+        return false;
+    }
+
+    // Set location to the piece(on the click function) and delete the other piece if piece he eat him
+    setLocation(row, col, piece) {
+        const isOccupied = this.getPiece(row, col);
+        console.log(this.turn)
+        if (isOccupied) {
+            isOccupied.deletePiece();
+        }
+        piece.MoveLocation(row, col);
+    }
+
+    // To decide who`s turn
+    nextTurn() {
+        this.turn = this.turn == WHITE_PLAYER ? BLACK_PLAYER : WHITE_PLAYER;
+    }
+
+}
+
+class state {
+    constructor(piece, cell) {
+        this.piece = piece;
+        this.cell = cell;
+
+    }
+
+    getCell() {
+        return this.cell
     }
 }
 
@@ -23,13 +64,13 @@ class Piece {
 
     appendPiece() {
         const cell = table.rows[this.row].cells[this.col];
-        if(this.player===WHITE_PLAYER){
+        if (this.player === WHITE_PLAYER) {
             cell.appendChild(this.pieceWhite);
         }
-        if(this.player===BLACK_PLAYER){
+        if (this.player === BLACK_PLAYER) {
             cell.appendChild(this.pieceBlack);
         }
-           
+
     }
 
     initializePiece() {
@@ -40,26 +81,174 @@ class Piece {
         this.appendPiece();
     }
 
-    // possibleMoves(){
+    changeLocation(row, col) {
+        this.row = row;
+        this.col = col;
+    }
 
-    // }
+    // Change the loction and the image of the piece
+    MoveLocation(row, col) {
+        this.changeLocation(row, col);
+        this.appendPiece();
+    }
+
+    // Delete piece from the board
+    deletePiece() {
+
+        this.pieceBlack.remove();
+        this.deleted = true;
+        this.row = -1;
+        this.col = -1;
+
+    }
+
+
+    possibleMoves() {
+        let filteredMoves = [];
+        if (this.player === BLACK_PLAYER) {
+            filteredMoves = this.blackMoves();
+        } else if (this.player === WHITE_PLAYER) {
+            filteredMoves = this.whiteMoves();
+        } else {
+            console.log("Unknown type");
+        }
+        return filteredMoves
+    }
+
+    isExist(row, col) {
+        return -1 < row && row < 8 && -1 < col && col < 8
+    }
+
+    blackMoves() {
+        let moves = [];
+        let locationOccupied;
+        let isOccupied;
+
+        let col = this.col;
+        let row = this.row;
+
+        locationOccupied = boardData.getPiece(row + 1, col + 1);
+        isOccupied = boardData.getPiece(row + 2, col + 2);
+        if (!locationOccupied && locationOccupied.player !== this.player) {
+            moves.push([row + 1, col + 1]);
+        }
+        if (locationOccupied && locationOccupied.player !== this.player && !isOccupied) {
+            moves.push([row + 2, col + 2]);
+        }
+
+        locationOccupied = boardData.getPiece(row + 1, col - 1);
+        isOccupied = boardData.getPiece(row + 2, col + 2);
+        if (!locationOccupied && locationOccupied.player !== this.player) {
+            moves.push([row + 1, col - 1]);
+        }
+
+        if (locationOccupied && locationOccupied.player !== this.player && !isOccupied) {
+            moves.push([row + 2, col - 2]);
+        }
+
+        moves = moves.filter((move) => {
+            if (this.isExist(move[0], move[1])) {
+                return true;
+            }
+        })
+        return moves
+    }
+
+    whiteMoves() {
+        let moves = [];
+        let locationOccupied;
+        let isOccupied;
+
+        let col = this.col;
+        let row = this.row;
+
+        locationOccupied = boardData.getPiece(row - 1, col + 1);
+        isOccupied = boardData.getPiece(row - 2, col + 2);
+        if (!locationOccupied && locationOccupied.player !== this.player) {
+            moves.push([row - 1, col + 1]);
+        }
+
+        if (locationOccupied && locationOccupied.player !== this.player && !isOccupied) {
+            moves.push([row - 2, col + 2]);
+        }
+
+        locationOccupied = boardData.getPiece(row - 1, col - 1);
+        isOccupied = boardData.getPiece(row - 2, col - 2);
+        if (!locationOccupied && locationOccupied.player !== this.player) {
+            moves.push([row - 1, col - 1]);
+        }
+
+        if (locationOccupied && locationOccupied.player !== this.player && !isOccupied) {
+            moves.push([row - 2, col - 2]);
+        }
+
+        moves = moves.filter((move) => {
+            if (this.isExist(move[0], move[1])) {
+                return true;
+            }
+        })
+        return moves
+    }
+
 }
 
 
 // When you click on some piece
-function onCellClick(event , row ,col){
-// Delete the selected cell when you select another cell
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      table.rows[i].cells[j].classList.remove('possible-move');
-      table.rows[i].cells[j].classList.remove('selected');
-    }
-  }
+function onCellClick(event, row, col) {
 
-  // background to the selected cell
-  selectedCell = event.currentTarget;
-  selectedCell.classList.add('selected');
+    // Move the pieces
+    if (pieceOld !== null) {
+        let newLocation = event.currentTarget;
+        if (newLocation.classList.contains('possible-move')) {
+            boardData.setLocation(row, col, pieceOld.piece);
+        }
+        pieceOld = null;
+    }
+
+    // Delete the selected cell when you select another cell
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            table.rows[i].cells[j].classList.remove('possible-move');
+            table.rows[i].cells[j].classList.remove('selected');
+        }
+    }
+
+    // background to the selected cell
+    selectedCell = event.currentTarget;
+    selectedCell.classList.add('selected');
+
+    boardData.nextTurn()
+    // Show possible moves to the black player when it`s turn
+    for (let piece of boardData.pieces) {
+        if (piece.row === row && piece.col === col && !piece.deleted) {
+            if (piece.player === WHITE_PLAYER ) {
+                pieceOld = new state(piece, selectedCell);
+                console.log(pieceOld)
+                let possibleMoves = piece.possibleMoves();
+                for (let possibleMove of possibleMoves) {
+                    table.rows[possibleMove[0]].cells[possibleMove[1]].classList.add('possible-move');
+                }
+            }
+        }
+    }
+
+    for (let piece of boardData.pieces) {
+        if (piece.row === row && piece.col === col && !piece.deleted) {
+            if (piece.player === BLACK_PLAYER) {
+                pieceOld = new state(piece, selectedCell);
+                console.log(pieceOld)
+                let possibleMoves = piece.possibleMoves();
+                for (let possibleMove of possibleMoves) {
+                    table.rows[possibleMove[0]].cells[possibleMove[1]].classList.add('possible-move');
+                }
+            }
+        }
+    }
+
 }
+
+
+
 
 
 // Add the pieces
@@ -67,11 +256,11 @@ function piecesOnBoard() {
     let pieces = [];
     for (i = 0; i < 8; i += 2) {
         pieces.push(new Piece(0, i, BLACK_PLAYER))
-        pieces.push(new Piece(1, i+1, BLACK_PLAYER))
+        pieces.push(new Piece(1, i + 1, BLACK_PLAYER))
         pieces.push(new Piece(2, i, BLACK_PLAYER))
-        pieces.push(new Piece(5, i+1, WHITE_PLAYER))
+        pieces.push(new Piece(5, i + 1, WHITE_PLAYER))
         pieces.push(new Piece(6, i, WHITE_PLAYER))
-        pieces.push(new Piece(7, i+1, WHITE_PLAYER))
+        pieces.push(new Piece(7, i + 1, WHITE_PLAYER))
     }
     return pieces;
 }
@@ -85,6 +274,20 @@ function createCheckersBoard() {
     background = document.createElement('div');
     document.body.appendChild(background);
     background.classList.add("background");
+
+    // Create a title
+    heading = document.createElement('H1');
+    textNode = document.createTextNode("Chechers game");
+    heading.appendChild(textNode);
+    background.appendChild(heading);
+    heading.classList.add("h1");
+
+    // Create white turn/black turn
+    turnText = document.createElement('H1');
+    textNodeTurnText = document.createTextNode("white turn");
+    turnText.appendChild(textNodeTurnText);
+    background.appendChild(turnText);
+    turnText.classList.add("turn");
 
     table = document.createElement('table');
     table.className = "table";
